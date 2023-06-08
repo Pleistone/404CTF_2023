cdcfbcc00b2dc6040330c05306eaeab1846d3402
Pleistone <pleistone.mage@gmail.com>
2023-06-08 11:40:00 +0200

    ajuste

--- Cryptographie/Dessine_moi_une_courbe_elliptique/solve.py
+++ Cryptographie/Dessine_moi_une_courbe_elliptique/solve.py
@@ -1,40 +1,14 @@
 from Crypto.Cipher import AES
 import hashlib
 
-# Cherche l'inverse modulaire de a modulo n. C'est-à-dire le nombre b tel que a*b congrue à 1 modulo n.
-def inverse_modulaire(a, n):
-    # Appliquer l'algorithme d'Euclide étendu
-    r, r_prev = n, a
-    s, s_prev = 0, 1
-
-    while r != 0:
-        quotient = r_prev // r
-        r, r_prev = r_prev - quotient * r, r
-        s, s_prev = s_prev - quotient * s, s
-
-    # Vérifier si l'inverse modulaire existe
-    if r_prev != 1:
-        raise ValueError("L'inverse modulaire n'existe pas.")
-    
-    # Normaliser l'inverse modulaire dans l'intervalle [0, n-1]
-    inverse = s_prev % n
-    return inverse
 
 # Détermine l'équation d'une courbe elliptique de la forme y^2 = x^3 + a*x +b
 def get_AES_key(x1,y1,x2,y2,p):
-    r1 = (y1**2-y2**2)%p
-    r2 = (x1**3-x2**3)%p
-    r3 = (r1-r2)%p
+    a = (y1**2 - y2**2 - (x1**3 - x2**3)) * pow((x1 -x2), -1, p)
+    a %= p
 
-    d = (x1-x2)%p
-    i = inverse_modulaire(d, p)
-    a = (r3*i)%p
-    print("a =",a)
-
-    r1 = (y2**2-x2**3)%p
-    r2 = (a*x2)%p
-    b = (r1-r2)%p
-    print("b =",b)
+    b = y1**2 - x1**3 - a*x1  
+    b %= p 
 
     key = str(a) + str(b)
     print("key =",key)
